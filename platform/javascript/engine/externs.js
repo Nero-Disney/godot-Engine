b87db4e54e64c78ed57078e32e40e41543ae60f5
Fabio Alessandrelli <fabio.alessandrelli@gmail.com>
2020-03-24 09:31:39 +0100

    Add clousure compiler externs for BrowserFS

--- platform/javascript/engine/externs.js
+++ platform/javascript/engine/externs.js
@@ -1,3 +1,6 @@
 var Godot;
 var WebAssembly = {};
 WebAssembly.instantiate = function(buffer, imports) {};
+var BrowserFS = {};
+BrowserFS.configure = function(config, callback) {};
+BrowserFS.EmscriptenFS = /** @constructor */ function(fs, path, errno) {};
